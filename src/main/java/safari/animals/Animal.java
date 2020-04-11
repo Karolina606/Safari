a01a93c402ce91d81750095c02c2efcc7e9df6d9
Karolina Nogacka <nogacka.karolina@gmail.com>
2020-04-11 17:13:06 +0200

    add classDiagram.puml

--- src/main/java/safari/animals/Animal.java
+++ src/main/java/safari/animals/Animal.java
@@ -4,8 +4,12 @@ public abstract class Animal {
     //energy level 0-10
     int energyLevel = 10;
     boolean isAsleep;
+    int positionX;
+    int positionY;
 
-    public abstract void makeMove();
-
-
+    public Animal(int positionX, int positionY){
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+    public abstract void move(int horizontalMax, int verticalMax);
 }
--- src/main/java/safari/animals/Zebra.java
+++ src/main/java/safari/animals/Zebra.java
@@ -1,4 +1,13 @@
 package safari.animals;
 
-public class Zebra extends Animal{
+public class Zebra<quantity> extends Animal{
+
+    public Zebra(int positionX, int positionY){
+        super(positionX, positionY);
+    }
+
+    public void move(int horizontalMax, int verticalMax){
+
+    }
+
 }
