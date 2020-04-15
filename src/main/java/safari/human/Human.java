2cebc92e907ef74f08ef68ea033a6f7783a42213
Karolina Nogacka <nogacka.karolina@gmail.com>
2020-04-15 20:53:15 +0200

    write Human calss methods and add javadoc

--- src/main/java/safari/human/Human.java
+++ src/main/java/safari/human/Human.java
@@ -1,4 +1,42 @@
 package safari.human;
 
-public class Human {
+import safari.Position;
+import safari.Safari;
+import safari.SafariObject;
+import safari.animals.Animal;
+import safari.animals.Lion;
+
+import java.util.List;
+
+/**
+ * Class to handle with Human objects
+ */
+public class Human extends SafariObject {
+    private Gun gun;
+
+    /**
+     *Makes a Human and places it on Safari on pointed Position
+     * @param position Postion to place Human in
+     * @param safari Safari he lives on
+     */
+    public Human(Position position, Safari safari){
+        safari.getMap().placeSafariObject(this, position, safari);
+        gun = new Gun();
+    }
+
+    /**
+     * Searchs for Lion on the Safari
+     * @param listOfAnimals list of all Animals on the Safari
+     * @return Lion's Position if Lion is on the Safari, returns new Point (-1, -1) if it is not there
+     */
+    private Position lookForTheAim(List<Animal> listOfAnimals){
+        //szuka lwa na liście wszystkich zwierząt
+        for(Animal animal: listOfAnimals){
+            if(animal instanceof Lion){
+                return animal.getPosition();
+            }
+        }
+        //kiedy nie ma żadnego lwa na safari
+        return new Position(-1, -1);
+    }
 }
