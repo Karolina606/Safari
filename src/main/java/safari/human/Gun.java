707f30f59ec6f8d9a22c360bd12876c3e9d9d15c
Karolina Nogacka <nogacka.karolina@gmail.com>
2020-04-15 20:38:04 +0200

    add javadoc comments to Gun class

--- src/main/java/safari/human/Gun.java
+++ src/main/java/safari/human/Gun.java
@@ -1,4 +1,40 @@
 package safari.human;
 
+import safari.Position;
+import safari.Safari;
+import safari.SafariMap;
+
+/**
+ * Class to deal with gun
+ */
 public class Gun {
+    private int quantityOfBullets;
+
+    /**
+     *Makes Gun object and sets it's quantityOfBullets to 10
+     */
+    public Gun(){
+        quantityOfBullets = 10;
+    }
+
+    /**
+     * Fires the gun to kill Lion
+     * it will kill a Lion if it lives on Safari and when there is amunition left
+     * @param position position of a Lion
+     * @param safari safari where a Lion lives
+     */
+    public void shoot(Position position, Safari safari){
+        //jesli nie ma wiecej amunicji
+        if(quantityOfBullets <= 0){
+            System.out.println("There is no more ammunition");
+        }
+        //jesli nie ma lwa na safari
+        else if(position.getX() == -1 && position.getY() == -1){
+            System.out.println("There is no lion to hunt");
+        }
+        //jesli na safari jest lew
+        else{
+            safari.getMap().freeUpPosition(position, safari);
+        }
+    }
 }
